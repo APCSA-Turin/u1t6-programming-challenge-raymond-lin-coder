ddfb6c130e9cde573bbd186a6636aefd04a1f449
Raymond Lin <raymondl183@nycstudents.net>
2025-10-07 17:53:10 +0000

    First commit

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,13 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
-
+        Calculator calc1 = new Calculator("Skibidi");
         //2. call any methods of that class to test
-
+        System.out.println(calc1.performOperation("*", 1, 3));
+        System.out.println(calc1.divisibleBy(4, 3));
+        System.out.println(calc1.coordinatePair(4, 3));
+        System.out.println(calc1.absoluteValue(4, 4));
+        System.out.println(calc1.average(5, 8));
+        System.out.println(calc1.info());
     }
 }
